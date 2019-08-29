bfe940e49d089ffdfd959c5f534b8a4c4f4c2183
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2019-08-29 16:29:53 +0300

    works

--- src/cli.js
+++ src/cli.js
@@ -30,13 +30,61 @@ import transform from './transform';
 import createValidator from './validate';
 import createRecordValidator from './validate-record';
 import {Transformer} from '@natlibfi/melinda-record-import-commons';
+import moment from 'moment';
+import path from 'path';
 
 const {runCLI} = Transformer;
 
 run();
 
 async function run() {
-	runCLI({name: 'melinda-record-import-transformer-helmet', transformStream});
+	const yargs = [
+		{option: 'v', conf: {alias: 'validate', default: false, type: 'boolean', describe: 'Validate records'}},
+		{option: 'f', conf: {alias: 'fix', default: false, type: 'boolean', describe: 'Validate & fix records'}}
+	];
+	const name = 'melinda-record-import-transformer-helmet';
+	runCLI(name, yargs, readArgs);
+
+	async function readArgs(stream, args, spinner, fs) {
+		const records = await transformStream(stream, args.validate, args.fix);
+
+		if (args.validate || args.fix) {
+			spinner.succeed();
+			spinner.start('Validating records');
+
+			const invalidCount = records.filter(r => r.failed).length;
+			const validCount = records.length - invalidCount;
+			spinner.succeed(`Validating records (Valid: ${validCount}, invalid: ${invalidCount})`);
+
+			if (args.recordsOnly) {
+				console.error(`Excluding ${records.filter(r => r.failed).length} failed records`);
+				handleRecordsOutput(records.filter(r => !r.failed).map(r => r.record));
+			} else {
+				console.log(JSON.stringify(records.map(r => {
+					return {record: r.record.toObject(), timestamp: moment(), ...r};
+				}), undefined, 2));
+			}
+		} else {
+			spinner.succeed();
+			handleRecordsOutput(records);
+		}
+
+		function handleRecordsOutput(records) {
+			if (args.outputDirectory) {
+				if (!fs.existsSync(args.outputDirectory)) {
+					fs.mkdirSync(args.outputDirectory);
+				}
+
+				records
+					.forEach((record, index) => {
+						const file = path.join(args.outputDirectory, `${index}.json`);
+						fs.writeFileSync(file, JSON.stringify(record.toObject(), undefined, 2));
+					});
+			} else {
+				console.log(JSON.stringify(records.map(r => r.toObject()), undefined, 2));
+			}
+		}
+	}
 
 	async function transformStream(stream, argsValidate, argsFix) {
 		const records = await transform(stream);
