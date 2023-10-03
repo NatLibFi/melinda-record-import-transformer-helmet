f78500826d7d588ffd1d8d3c6bbf74d69bf2fae6
natlibesa <31346486+natlibesa@users.noreply.github.com>
2023-10-03 13:31:43 +0300

    MRI-217-D: order checked for non-standard but often used subfields 5 and 9

--- src/transform/convert/generate0XXFields.js
+++ src/transform/convert/generate0XXFields.js
@@ -18,7 +18,8 @@ export function handle020(marcRecord) {
 }
 
 export function handle028(marcRecord) {
-  // subfield alternatives: a, b, q, 6, 8 -> presentation order (fin): b, a, q, 6, 8
+  // Official subfield alternatives: a, b, q, 6, 8 -> presentation order (fin): b, a, q, 6, 8
+  // non-standard but in use: $5 ja $9
   const arrFoundFields028 = marcRecord.get(/^028$/u);
 
   if (!arrFoundFields028) {
@@ -31,9 +32,12 @@ export function handle028(marcRecord) {
     const aSubfield = field.subfields.filter(sf => sf.code === 'a'); // eslint-disable-line functional/immutable-data
     const bSubfield = field.subfields.filter(sf => sf.code === 'b'); // eslint-disable-line functional/immutable-data
     const qSubfield = field.subfields.filter(sf => sf.code === 'q'); // eslint-disable-line functional/immutable-data
+    const nr5Subfield = field.subfields.filter(sf => sf.code === '5'); // eslint-disable-line functional/immutable-data
     const nr6Subfield = field.subfields.filter(sf => sf.code === '6'); // eslint-disable-line functional/immutable-data
+    const nr7Subfield = field.subfields.filter(sf => sf.code === '7'); // eslint-disable-line functional/immutable-data
     const nr8Subfield = field.subfields.filter(sf => sf.code === '8'); // eslint-disable-line functional/immutable-data
-    const otherSubfield = field.subfields.filter(sf => !['a', 'b', 'q', '6', '8'].includes(sf.code)); // eslint-disable-line functional/immutable-data
+    const nr9Subfield = field.subfields.filter(sf => sf.code === '9'); // eslint-disable-line functional/immutable-data
+    const otherSubfield = field.subfields.filter(sf => !['a', 'b', 'q', '5', '6', '7', '8', '9'].includes(sf.code)); // eslint-disable-line functional/immutable-data
 
     marcRecord.removeField(field);
 
@@ -47,7 +51,7 @@ export function handle028(marcRecord) {
     marcRecord.insertField(newField);
 
     function buildNewSubfields () {
-      const newSubs = [...bSubfield, ...aSubfield, ...qSubfield, ...nr6Subfield, ...nr8Subfield, ...otherSubfield];
+      const newSubs = [...bSubfield, ...aSubfield, ...qSubfield, ...nr5Subfield, ...nr6Subfield, ...nr7Subfield, ...nr8Subfield, ...nr9Subfield, ...otherSubfield];
       return newSubs;
     }
 
