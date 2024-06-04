576408e12e3e5e156f6d6672938a964adcd39da3
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2024-06-04 13:33:26 +0300

    add mongoOperator

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ import {parseBoolean} from '@natlibfi/melinda-commons';
 
 export const profileIds = readEnvironmentVariable('PROFILE_IDS', {defaultValue: ['foobar'], format: v => JSON.parse(v)});
 export const amqpUrl = readEnvironmentVariable('AMQP_URL', {defaultValue: 'amqp://127.0.0.1:5672/'});
+export const mongoUrl = readEnvironmentVariable('MONGO_URI', {defaultValue: false});
 export const abortOnInvalidRecords = readEnvironmentVariable('ABORT_ON_INVALID_RECORDS', {defaultValue: false, format: parseBoolean});
 
 export const recordImportApiOptions = {
