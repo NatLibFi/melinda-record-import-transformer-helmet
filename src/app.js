a683b2e135d44937494538f0bef403a3d7c1e317
Joni Ollila <53558796+natlibfi-jonollil@users.noreply.github.com>
2024-10-10 14:31:39 +0300

    New Mongo logic (#241)
    * new mongo operator
    * remove unnessessary envs
    * update-deps

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 import {createLogger} from '@natlibfi/melinda-backend-commons';
-import {transformerBlobLogic, createApiClient as createRecordImportApiClient, createMongoOperator} from '@natlibfi/melinda-record-import-commons';
+import {transformerBlobLogic, createMongoBlobsOperator} from '@natlibfi/melinda-record-import-commons';
 import createTransformHandler from './transform';
 import amqplib from 'amqplib';
 
 export async function startApp(config) {
   const logger = createLogger();
-  const mongoOperator = config.mongoUrl ? await createMongoOperator(config.mongoUrl) : false;
-  const riApiClient = await createRecordImportApiClient(config.recordImportApiOptions, config.keycloakOptions, mongoOperator);
+  const mongoOperator = await createMongoBlobsOperator(config.mongoUrl);
+
   const transformHandler = createTransformHandler();
 
   logger.info('Starting melinda record import transformer helmet');
   try {
-    await transformerBlobLogic(riApiClient, transformHandler, amqplib, config);
+    await transformerBlobLogic(mongoOperator, transformHandler, amqplib, config);
   } catch (error) {
     logger.error(error);
   }
--- src/config.js
+++ src/config.js
@@ -5,15 +5,3 @@ export const profileIds = readEnvironmentVariable('PROFILE_IDS', {defaultValue: 
 export const amqpUrl = readEnvironmentVariable('AMQP_URL', {defaultValue: 'amqp://127.0.0.1:5672/'});
 export const mongoUrl = readEnvironmentVariable('MONGO_URI', {defaultValue: false});
 export const abortOnInvalidRecords = readEnvironmentVariable('ABORT_ON_INVALID_RECORDS', {defaultValue: false, format: parseBoolean});
-
-export const recordImportApiOptions = {
-  recordImportApiUrl: readEnvironmentVariable('RECORD_IMPORT_API_URL', {defaultValue: 'cli'}),
-  userAgent: readEnvironmentVariable('API_CLIENT_USER_AGENT', {defaultValue: '_RECORD-IMPORT-TRANSFORMER'}),
-  allowSelfSignedApiCert: readEnvironmentVariable('ALLOW_API_SELF_SIGNED', {defaultValue: false, format: parseBoolean})
-};
-
-export const keycloakOptions = {
-  issuerBaseURL: readEnvironmentVariable('KEYCLOAK_ISSUER_BASE_URL', {defaultValue: 'KEYCLOAK_ISSUER_BASE_URL env is not set!'}),
-  serviceClientID: readEnvironmentVariable('KEYCLOAK_SERVICE_CLIENT_ID', {defaultValue: 'KEYCLOAK_SERVICE_CLIENT_ID env is not set!'}),
-  serviceClientSecret: readEnvironmentVariable('KEYCLOAK_SERVICE_CLIENT_SECRET', {defaultValue: 'KEYCLOAK_SERVICE_CLIENT_SECRET env is not set!'})
-};
