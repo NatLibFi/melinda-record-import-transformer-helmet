43cff346a51e42fd881e55231501ce20e154e0cf
Joni Ollila <53558796+natlibfi-jonollil@users.noreply.github.com>
2025-04-16 08:19:46 +0300

    lookup to main (#243)
    * lookup and fixes (#242)
    * update
    * update-node
    * swap validator order

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 import {createLogger} from '@natlibfi/melinda-backend-commons';
-import {transformerBlobLogic, createMongoBlobsOperator} from '@natlibfi/melinda-record-import-commons';
+import {transformerBlobLogic, createMongoBlobsOperator, createAmqpOperator} from '@natlibfi/melinda-record-import-commons';
 import createTransformHandler from './transform';
 import amqplib from 'amqplib';
 
 export async function startApp(config) {
   const logger = createLogger();
   const mongoOperator = await createMongoBlobsOperator(config.mongoUrl);
-
+  const amqpOperator = await createAmqpOperator(amqplib, config.amqpUrl);
   const transformHandler = createTransformHandler();
 
   logger.info('Starting melinda record import transformer helmet');
   try {
-    await transformerBlobLogic(mongoOperator, transformHandler, amqplib, config);
+    await transformerBlobLogic(mongoOperator, amqpOperator, transformHandler, config);
   } catch (error) {
     logger.error(error);
+  } finally {
+    // debugHandling(`Closing AMQP resources!`);
+    await amqpOperator.closeChannel();
+    await amqpOperator.closeConnection();
+    mongoOperator.closeClient();
   }
 }
--- src/validate/index.js
+++ src/validate/index.js
@@ -1,31 +1,3 @@
-/**
-*
-* @licstart  The following is the entire license notice for the JavaScript code in this file.
-*
-* Helmet record transformer for the Melinda record batch import system
-*
-* Copyright (c) 2018-2019 University Of Helsinki (The National Library Of Finland)
-*
-* This file is part of melinda-record-import-transformer-helmet
-*
-* melinda-record-import-transformer-helmet program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* melinda-record-import-transformer-helmet is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-* @licend  The above is the entire license notice
-* for the JavaScript code in this file.
-*
-*/
-
 /* eslint-disable new-cap */
 import validateFactory from '@natlibfi/marc-record-validate';
 import {
@@ -47,6 +19,7 @@ import {
 
 export default async () => {
   const validate = validateFactory([
+    await EmptyFields(),
     await FieldsPresent([/^(020|022|024)$/u]), // eslint-disable-line
     await FieldsPresent([/^336$/u, /^337$/u, /^338$/u]),
     await FieldExclusion([
@@ -66,7 +39,6 @@ export default async () => {
     await SanitizeVocabularySourceCodes(),
     await Field505Separators(),
     await TypeOfDateF008(),
-    await EmptyFields(),
     await IsbnIssn({hyphenateISBN: true}),
     await Field521Fix(),
     await SubfieldExclusion([{tag: /^041$/u, subfields: [{code: /a|d/u, value: /^zxx$/u}]}]),
