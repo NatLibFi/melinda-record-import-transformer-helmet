173856242d524e86fcc44333bf1e0078999b3521
nvolk <nicholas.volk@helsinki.fi>
2025-11-18 17:31:01 +0200

    Tests for field 130

---
+++ src/transform/convert/generate1XXFields.test.js
@@ -0,0 +1,88 @@
+import createDebugLogger from 'debug';
+import assert from 'node:assert/strict';
+import {READERS} from '@natlibfi/fixura';
+import generateTests from '@natlibfi/fixugen';
+import {Error as TransformationError} from '@natlibfi/melinda-commons';
+import {MarcRecord} from '@natlibfi/marc-record';
+import {handle130} from './generate1XXFields.js';
+
+const debug = createDebugLogger('@natlibfi/tests/melinda-record-import-transformer-helmet/transform/convert:generate1XXFields');
+
+generateTests({
+  callback,
+  path: [import.meta.dirname, '..', '..', '..', 'test-fixtures', 'generate1XXFields'],
+  recurse: true,
+  useMetadataFile: true,
+  fixura: {
+    failWhenNotFound: true,
+    reader: READERS.JSON
+  }
+});
+
+/**
+ * Callback function for tests
+ * @date 11/10/2023 - 8:03:40 AM
+ *
+ * @param {{ getFixture: READER, functionToUse: string, expectedError?: boolean, expectedErrorStatus?: string }} metadataParams From metadata.json
+ * @param {READER} metadataParams.getFixture Auto generated file reader
+ * @param {string} metadataParams.functionToUse
+ * @param {boolean} [metadataParams.expectedError=false]
+ * @param {string} [metadataParams.expectedErrorStatus='200']
+ */
+function callback({
+  getFixture,
+  functionToUse,
+  expectedError = false,
+  expectedErrorStatus = '200'
+}) {
+  const inputData = new MarcRecord(getFixture('input.json'));
+  const expectedResults = getFixture('output.json');
+  const result = handleProcess(functionToUse, inputData, expectedError, expectedErrorStatus); // eslint-disable-line
+
+  if (result) {
+    assert.deepStrictEqual(result, expectedResults);
+    return;
+  }
+
+
+  /**
+   * Test process handling. Handles normal and error cases
+   * @date 12/04/2024 - 8:30:00 AM
+   *
+   * @param {string} functionToUse Name of function for testing
+   * @param {MarcRecord} inputData Input record
+   * @param {boolean} expectedError Is error expected from this test
+   * @param {string} expectedErrorStatus What is status of error that is expected
+   * @returns {object|false} Marc record result object or false
+   */
+  function handleProcess(functionToUse, inputData, expectedError, expectedErrorStatus) {
+    try {
+      if (functionToUse === 'handle130') {
+        handle130(inputData);
+        //const result = inputData.insertFields(handle130(inputData)).toObject();
+        return inputData.toObject();
+      }
+
+      throw new Error('Invalid function name!');
+    } catch (err) {
+      const debugErrorHandling = debug.extend('errorHandling');
+      debugErrorHandling(err);
+
+      if (expectedError) {
+        assert(err instanceof Error);
+
+        if (err instanceof TransformationError) { // specified error
+          assert.match(err.payload, new RegExp(expectedError, 'u'));
+          assert.match(err.status, new RegExp(expectedErrorStatus, 'u'));
+          return false;
+        }
+
+        // common error
+        assert.match(err.message, new RegExp(expectedError, 'u'));
+        return false;
+      }
+
+      throw err;
+    }
+  }
+}
